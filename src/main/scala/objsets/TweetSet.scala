860ed68864d0a18c729e29e0732dd34b1101ceac
Christian Kleinboelting <kleinboelting@signal7.de>
2015-10-02 16:40:34 +0200

    Add union and filter implementation

--- src/main/scala/objsets/TweetSet.scala
+++ src/main/scala/objsets/TweetSet.scala
@@ -42,7 +42,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: (Tweet) => Boolean): TweetSet = filterAcc(p, this)
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -55,7 +55,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
+  def union(that: TweetSet): TweetSet
   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -108,8 +108,8 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
   
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = this
   /**
    * The following methods are already implemented
    */
@@ -121,13 +121,34 @@ class Empty extends TweetSet {
   def remove(tweet: Tweet): TweetSet = this
 
   def foreach(f: Tweet => Unit): Unit = ()
+
+  /**
+   * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
+   *
+   * Question: Should we implment this method here, or should it remain abstract
+   * and be implemented in the subclasses?
+   */
+  override  def union(that: TweetSet): TweetSet = that
+
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet =
+        if (p(elem))
+          new NonEmpty(elem, left.filter(p), right.filter(p))
+        else
+          remove(elem).filter(p)
+
+  /**
+   * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
+   *
+   * Question: Should we implment this method here, or should it remain abstract
+   * and be implemented in the subclasses?
+   */
+  override def union(that: TweetSet): TweetSet =
+    (left.union(right)).union(that).incl(elem)
+
   /**
    * The following methods are already implemented
    */
